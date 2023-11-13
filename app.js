87ce383f156c9b209bff254f18729ddd0e4e9b00
Jiawen Gu <gu.jiawen@northeastern.edu>
2023-11-12 16:17:18 -0800

    5.1

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import Hello from './hello.js';
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import "dotenv/config";
+
+import cors from "cors";
+const app = express()
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+
+app.use(express.json());
+Hello(app)
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app)
+app.listen(process.env.PORT || 4000);
+app.listen(4000)
