92e958811d02671cd9e53132d1f79832476ec9ca
Jiawen Gu <gu.jiawen@northeastern.edu>
2023-11-27 01:08:08 -0800

    4 finish

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ import "dotenv/config";
 
 
 import cors from "cors";
+import UserRoutes from "./users/routes.js";
 import mongoose from 'mongoose';
-mongoose.connect("mongodb://localhost:27017/kanbas")
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas")
 const app = express()
 app.use(
   cors({
@@ -19,9 +20,10 @@ app.use(
 );
 
 app.use(express.json());
+UserRoutes(app);
 Hello(app)
 CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app)
-app.listen(process.env.PORT || 4000);
-// app.listen(4000)
+// app.listen(process.env.PORT || 4000);
+app.listen(4000)
---
+++ users/routes.js
@@ -0,0 +1,110 @@
+import * as dao from "./dao.js";
+let currentUser = null;
+function UserRoutes(app) {
+  const createUser = async (req, res) => {
+    const {username, password,firstName,lastName, role} = req.body;
+    const user = await dao.createUser({
+      username, password, firstName, lastName, role
+    })
+    res.json(user);
+  };
+
+  const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    const status = await dao.deleteUser(id);
+    res.json(status);
+  };
+
+  const findAllUsers = async (req, res) => {
+    const users = await dao.findAllUsers();
+    res.json(users);
+  };
+
+  const findUserById = async (req, res) => {
+    const id = req.params.id;
+    const user = await dao.findUserById(id);
+    res.json(user);
+  };
+
+  const findUserByUsername = async (req, res) => { 
+    const username = req.params.username;
+    const user = await dao.findUserByUsername(username);
+    res.json(user);
+  };
+
+  const findUserByCredentials = async (req, res) => { 
+    const {username,password} = req.params;
+    const user = await dao.findUserByCredentials(username,password);
+    res.json(user);
+  };
+
+  const findUsersByRole = async (req, res) => { 
+    const role = req.params.role;
+    const users = await dao.findUsersByRole(role);
+    res.json(users);
+  };
+
+  const updateFirstName = async (req, res) => { 
+    const id = req.params.id;
+    const newFirstName = req.params.newFirstName;
+    const status = await dao.updateUser(id,{firstName: newFirstName});
+    res.json(status);
+  };
+
+  const updateUser = async (req, res) => { 
+    const { userId } = req.params;
+    const status = await dao.updateUser(userId, req.body);
+    currentUser = await dao.findUserById(userId);
+    res.json(status);
+  };
+
+  const signup = async (req, res) => { 
+    const user = await dao.findUserByUsername(
+      req.body.username);
+    if (user) {
+      res.status(400).json(
+        { message: "Username already taken" });
+    }
+    currentUser = await dao.createUser(req.body);
+    res.json(currentUser);
+  };
+
+  const signin = async (req, res) => { 
+    const { username, password } = req.body;
+    currentUser = await dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      res.json(currentUser);
+    } else {
+      res.status(403).json(
+        { message: 'Username or password incorrect' }
+      )
+    }
+
+  };
+  const signout = (req, res) => { 
+    currentUser = null;
+    res.json(200);
+  };
+  const account = async (req, res) => { 
+    res.json(currentUser);
+  };
+
+  app.get("/api/users", findAllUsers);
+  app.get("/api/users/:id", findUserById);
+  app.get("/api/users/username/:username",findUserByUsername)
+  app.get("/api/users/credentials/:username/:password", findUserByCredentials)
+  app.get("/api/users/role/:role",findUsersByRole)
+  // app.get("/api/users/:username/:password/:email/:role",createUser); 
+  app.get("/api/users/updateFirstName/:id/:newFirstName",updateFirstName);
+  app.get("/api/users/delete/:id", deleteUser);
+
+  app.delete("/api/users/:id", deleteUser);
+
+  app.post("/api/users", createUser);
+  app.put("/api/users/:id", updateUser);
+  app.post("/api/users/signup", signup);
+  app.post("/api/users/signin", signin);
+  app.post("/api/users/signout", signout);
+  app.post("/api/users/account", account);
+}
+export default UserRoutes;
